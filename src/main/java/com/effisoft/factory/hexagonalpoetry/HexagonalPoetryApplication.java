64cc01441258c54d60755d125390ada3fe1f29ce
Ronan Le Roy <rleroy@effisoft-group.com>
2018-11-29 16:28:25 +0100

    Initial commit.

---
+++ src/main/java/com/effisoft/factory/hexagonalpoetry/HexagonalPoetryApplication.java
@@ -0,0 +1,17 @@
+package com.effisoft.factory.hexagonalpoetry;
+
+import java.util.Scanner;
+
+public class HexagonalPoetryApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Here is some...");
+        System.out.println("I want to sleep");
+        System.out.println("Swat the files");
+        System.out.println("Softly, please");
+        System.out.println("-- Masaoka Shiki (1867 - 1902)");
+        System.out.println("Type enter to exit...");
+        new Scanner(System.in).nextLine();
+    }
+}
+
