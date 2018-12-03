6585b03408df51310a6a5ba2310ad1f692af28cf
Ronan Le Roy <rleroy@effisoft-group.com>
2018-12-03 16:09:50 +0100

    Domain evolution : change poem formatting.

--- src/main/java/com/leroy/ronan/hexagonalpoetry/domain/PoetryReader.java
+++ src/main/java/com/leroy/ronan/hexagonalpoetry/domain/PoetryReader.java
@@ -20,8 +20,33 @@ public class PoetryReader implements VersesProvider {
 
     public String giveMeSomePoetry() throws LibraryException {
         List<Poem> poems = library.getAllPoems();
-        return poems
-                .get(random.nextInt(poems.size()))
-                .getPoem();
+        return format(
+                poems.get(random.nextInt(poems.size()))
+        );
+    }
+
+    private String format(Poem poem) {
+        return formatTitle(poem) +
+                poem.getPoem() +
+                formatAuthorAndDate(poem);
+    }
+
+    private String formatTitle(Poem poem) {
+        String res = "";
+        if (poem.getTitle() != null) {
+            res = "== " + poem.getTitle() + " ==\n";
+        }
+        return res;
+    }
+
+    private String formatAuthorAndDate(Poem poem) {
+        String res = "";
+        if (poem.getAuthor() != null) {
+            res += "\n-- " + poem.getAuthor();
+            if (poem.getPublicationYear() != null) {
+                res += " (" + poem.getPublicationYear() + ")";
+            }
+        }
+        return res;
     }
 }
--- src/test/java/com/leroy/ronan/hexagonalpoetry/domain/PoetryReaderShould.java
+++ src/test/java/com/leroy/ronan/hexagonalpoetry/domain/PoetryReaderShould.java
@@ -13,8 +13,8 @@ public class PoetryReaderShould {
         PoetryLibrary library = () -> List.of(
                 new Poem(
                         "If you could read a leaf or tree\n" +
-                        "you’d have no need of books.\n" +
-                        "-- © Alistair Cockburn (1987)"
+                                "you’d have no need of books.\n" +
+                                "-- © Alistair Cockburn (1987)"
                 )
         );
         PoetryReader poetryReader = new PoetryReader(library);
@@ -24,8 +24,8 @@ public class PoetryReaderShould {
         Assert.assertEquals(
                 new Poem(
                         "If you could read a leaf or tree\n" +
-                        "you’d have no need of books.\n" +
-                        "-- © Alistair Cockburn (1987)"
+                                "you’d have no need of books.\n" +
+                                "-- © Alistair Cockburn (1987)"
                 ).getPoem(),
                 poem
         );
@@ -36,14 +36,14 @@ public class PoetryReaderShould {
         PoetryLibrary library = () -> List.of(
                 new Poem(
                         "If you could read a leaf or tree\n" +
-                        "you’d have no need of books.\n" +
-                        "-- © Alistair Cockburn (1987)"
+                                "you’d have no need of books.\n" +
+                                "-- © Alistair Cockburn (1987)"
                 ),
                 new Poem(
                         "I want to sleep\n" +
-                        "Swat the files\n" +
-                        "Softly, please.\n" +
-                        "-- Masaoka Shiki (1867 - 1902)."
+                                "Swat the files\n" +
+                                "Softly, please.\n" +
+                                "-- Masaoka Shiki (1867 - 1902)."
                 )
         );
         PoetryReader poetryReader = new PoetryReader(library, new Random(42L));
@@ -52,15 +52,52 @@ public class PoetryReaderShould {
 
         Assert.assertEquals(
                 "I want to sleep\n" +
-                "Swat the files\n" +
-                "Softly, please.\n" +
-                "-- Masaoka Shiki (1867 - 1902).",
+                        "Swat the files\n" +
+                        "Softly, please.\n" +
+                        "-- Masaoka Shiki (1867 - 1902).",
                 poem
         );
     }
 
     @Test
-    public void format_a_poem_using_its_title_author_and_date() {
+    public void format_a_poem_using_its_author_and_date() throws LibraryException {
+        PoetryLibrary library = () -> List.of(
+                new Poem(
+                        "Alistair Cockburn",
+                        null,
+                        "1987",
+                        "If you could read a leaf or tree\n" +
+                                "you’d have no need of books."
+                )
+        );
+        PoetryReader poetryReader = new PoetryReader(library);
 
+        String poem = poetryReader.giveMeSomePoetry();
+
+        Assert.assertEquals(
+                "If you could read a leaf or tree\n" +
+                "you’d have no need of books.\n" +
+                "-- Alistair Cockburn (1987)",
+                poem
+        );
+    }
+
+    @Test
+    public void format_a_poem_using_its_title() throws LibraryException {
+        PoetryLibrary library = () -> List.of(
+                new Poem(
+                        "Charles BAUDELAIRE",
+                        "L'albatros",
+                        null,
+                        "Souvent, pour s'amuser, les hommes d'équipage\nPrennent des albatros, vastes oiseaux des mers,\nQui suivent, indolents compagnons de voyage,\nLe navire glissant sur les gouffres amers.\n\nA peine les ont-ils déposés sur les planches,\nQue ces rois de l'azur, maladroits et honteux,\nLaissent piteusement leurs grandes ailes blanches\nComme des avirons traîner à côté d'eux.\n\nCe voyageur ailé, comme il est gauche et veule !\nLui, naguère si beau, qu'il est comique et laid !\nL'un agace son bec avec un brûle-gueule,\nL'autre mime en boitant, l'infirme qui volait !\n\nLe Poète est semblable au prince des nuées\nQui hante la tempête et se rit de l'archer ;\nExilé sur le sol au milieu des huées,\nSes ailes de géant l'empêchent de marcher."
+                )
+        );
+        PoetryReader poetryReader = new PoetryReader(library);
+
+        String poem = poetryReader.giveMeSomePoetry();
+
+        Assert.assertTrue(poem.startsWith("== L'albatros ==\n"));
+        Assert.assertTrue(poem.contains("\nPrennent des albatros, vastes oiseaux des mers,\n"));
+        Assert.assertTrue(poem.endsWith("\n-- Charles BAUDELAIRE"));
     }
 }
