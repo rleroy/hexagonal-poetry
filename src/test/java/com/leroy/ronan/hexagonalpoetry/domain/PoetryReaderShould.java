fe8a2c94e263ee7a342de8110ea67fabac1fe3ef
Ronan Le Roy <rleroy@effisoft-group.com>
2018-12-03 15:30:06 +0100

    Improve test readability.

--- src/test/java/com/leroy/ronan/hexagonalpoetry/domain/PoetryReaderShould.java
+++ src/test/java/com/leroy/ronan/hexagonalpoetry/domain/PoetryReaderShould.java
@@ -8,29 +8,25 @@ import java.util.Random;
 
 public class PoetryReaderShould {
 
-    private static final Poem ALISTAIR_POEM = new Poem(
-            "If you could read a leaf or tree\n" +
-            "you’d have no need of books.\n" +
-            "-- © Alistair Cockburn (1987)"
-    );
-    private static final Poem MASAOKA_SHIKI_POEM = new Poem(
-            "I want to sleep\n" +
-            "Swat the files\n" +
-            "Softly, please.\n" +
-            "-- Masaoka Shiki (1867 - 1902)."
-    );
-
     @Test
     public void retrieve_poetry_from_library() throws LibraryException {
         PoetryLibrary library = () -> List.of(
-                ALISTAIR_POEM
+                new Poem(
+                        "If you could read a leaf or tree\n" +
+                        "you’d have no need of books.\n" +
+                        "-- © Alistair Cockburn (1987)"
+                )
         );
         PoetryReader poetryReader = new PoetryReader(library);
 
         String poem = poetryReader.giveMeSomePoetry();
 
         Assert.assertEquals(
-                ALISTAIR_POEM.getPoem(),
+                new Poem(
+                        "If you could read a leaf or tree\n" +
+                        "you’d have no need of books.\n" +
+                        "-- © Alistair Cockburn (1987)"
+                ).getPoem(),
                 poem
         );
     }
@@ -38,16 +34,33 @@ public class PoetryReaderShould {
     @Test
     public void retrieve_poetry_from_multi_poems_library() throws LibraryException {
         PoetryLibrary library = () -> List.of(
-                ALISTAIR_POEM,
-                MASAOKA_SHIKI_POEM
+                new Poem(
+                        "If you could read a leaf or tree\n" +
+                        "you’d have no need of books.\n" +
+                        "-- © Alistair Cockburn (1987)"
+                ),
+                new Poem(
+                        "I want to sleep\n" +
+                        "Swat the files\n" +
+                        "Softly, please.\n" +
+                        "-- Masaoka Shiki (1867 - 1902)."
+                )
         );
         PoetryReader poetryReader = new PoetryReader(library, new Random(42L));
 
         String poem = poetryReader.giveMeSomePoetry();
 
         Assert.assertEquals(
-                MASAOKA_SHIKI_POEM.getPoem(),
+                "I want to sleep\n" +
+                "Swat the files\n" +
+                "Softly, please.\n" +
+                "-- Masaoka Shiki (1867 - 1902).",
                 poem
         );
+    }
+
+    @Test
+    public void format_a_poem_using_its_title_author_and_date() {
+
     }
 }
